a9440c804642ea70e4f4e28d14c3ba121e185303
needhidevan <needhi.senthil@gmail.com>
2017-10-06 18:27:44 +0530

    third commit

--- app/src/main/java/com/example/smile/profitable_farming/Getinfo.java
+++ app/src/main/java/com/example/smile/profitable_farming/Getinfo.java
@@ -1,7 +1,9 @@
 package com.example.smile.profitable_farming;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Getinfo extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class Getinfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getinfo);
+        TextView t=(TextView)findViewById(R.id.textView2);
+        Typeface tf = Typeface.createFromAsset(this.getAssets(),"tamil/Bamini.ttf");
+        t.setTypeface(tf);
+        t.setText("இடம்:");
+
+        TextView t2=(TextView)findViewById(R.id.textView3);
+        Typeface tf1 = Typeface.createFromAsset(this.getAssets(),"tamil/Bamini.ttf");
+        t2.setTypeface(tf1);
+        t2.setText("ஈரப்பதம்:");
+
+        TextView t3=(TextView)findViewById(R.id.textView3);
+        Typeface tf2 = Typeface.createFromAsset(this.getAssets(),"tamil/Bamini.ttf");
+        t3.setTypeface(tf2);
+        t3.setText("மண்:");
+
+        
+
     }
 }
