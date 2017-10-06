5507f7cf2250986c8b1d71602bd3a4dc9a1ae83c
needhidevan <needhi.senthil@gmail.com>
2017-10-06 19:04:36 +0530

    tamil UI commit

--- app/src/main/java/com/example/smile/profitable_farming/Getinfo.java
+++ app/src/main/java/com/example/smile/profitable_farming/Getinfo.java
@@ -11,22 +11,6 @@ public class Getinfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getinfo);
-        TextView t=(TextView)findViewById(R.id.textView2);
-        Typeface tf = Typeface.createFromAsset(this.getAssets(),"tamil/Bamini.ttf");
-        t.setTypeface(tf);
-        t.setText("இடம்:");
-
-        TextView t2=(TextView)findViewById(R.id.textView3);
-        Typeface tf1 = Typeface.createFromAsset(this.getAssets(),"tamil/Bamini.ttf");
-        t2.setTypeface(tf1);
-        t2.setText("ஈரப்பதம்:");
-
-        TextView t3=(TextView)findViewById(R.id.textView3);
-        Typeface tf2 = Typeface.createFromAsset(this.getAssets(),"tamil/Bamini.ttf");
-        t3.setTypeface(tf2);
-        t3.setText("மண்:");
-
-        
 
     }
 }
