a782750fd4a33c7eb5903d4bc0b15ecac68e4f53
needhidevan <needhi.senthil@gmail.com>
2017-10-07 08:09:31 +0530

    locator commit

--- app/src/main/java/com/example/smile/profitable_farming/Getinfo.java
+++ app/src/main/java/com/example/smile/profitable_farming/Getinfo.java
@@ -1,4 +1,22 @@
 package com.example.smile.profitable_farming;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.List;
+import java.util.Locale;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -14,6 +32,10 @@ import android.widget.TextView;
 public class Getinfo extends AppCompatActivity {
 
     public Button btn;
+    Button button;
+    TextView textView;
+    private static final int MY_PERMISSION_REQUEST_LOCATION =1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +65,73 @@ public class Getinfo extends AppCompatActivity {
         }
 
         });
+        button= (Button) findViewById(R.id.button);
+        textView= (TextView) findViewById(R.id.textView);
+        button.setOnClickListener(new View.OnClickListener(){
+                                      @Override
+                                      public void onClick(View view) {
+                                          if(ContextCompat.checkSelfPermission(Getinfo.this,
+                                                  android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                                              if(ActivityCompat.shouldShowRequestPermissionRationale(Getinfo.this,
+                                                      android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                                                  ActivityCompat.requestPermissions(Getinfo.this,
+                                                          new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+                                              } else {
+                                                  ActivityCompat.requestPermissions(Getinfo.this,
+                                                          new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_LOCATION);
+                                              }
+                                          }else{
+                                              LocationManager locationManager= (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                                              Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                                              try{
+                                                  textView.setText(hereLocation(location.getLatitude(), location.getLongitude()));
+                                              }catch(Exception e){
+                                                  e.printStackTrace();
+                                                  Toast.makeText(Getinfo.this, "Not found!",Toast.LENGTH_SHORT).show();
+                                              }
+                                          }
+                                      }
+                                  }
+        );
     }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSION_REQUEST_LOCATION:{
+                if(grantResults.length> 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(Getinfo.this,
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                        LocationManager locationManager= (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                        try{
+                            textView.setText(hereLocation(location.getLatitude(), location.getLongitude()));
+                        }catch(Exception e){
+                            e.printStackTrace();
+                            Toast.makeText(Getinfo.this, "Not found!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else{
+                    Toast.makeText(this,"No permission granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    //get closet city name
+    public String hereLocation(double lat, double lon){
+        String curCity="";
+        Geocoder geocoder=new Geocoder(Getinfo.this, Locale.getDefault());
+        List<Address> addressList;
+        try
+        {
+            addressList= geocoder.getFromLocation(lat,lon,1);
+            if(addressList.size() > 0){
+                curCity=addressList.get(0).getLocality();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return curCity;
+    }
 }
+
+
--- app/src/main/java/com/example/smile/profitable_farming/second.java
+++ app/src/main/java/com/example/smile/profitable_farming/second.java
@@ -3,6 +3,7 @@ package com.example.smile.profitable_farming;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class second extends AppCompatActivity {
 
@@ -11,8 +12,16 @@ public class second extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Intent intent = getIntent();
-        String selectedRadioValue = intent.getStringExtra("selectedRadioValue");
+        String selectedRadioValue1 = intent.getStringExtra("selectedRadioValue");
         String selectedRadioValue2 = intent.getStringExtra("selectedRadioValue2");
+
+        TextView  textView1= (TextView) findViewById(R.id.textView8);
+        textView1.setText(selectedRadioValue1);
+
+        TextView  textView2= (TextView) findViewById(R.id.textView9);
+        textView2.setText(selectedRadioValue2);
+
+
 
 
 
